7c0a7c7a2a4e66528742f3630327b35aa6179ca5
Doxygen CI <ci-arduino@invalid>
2022-08-01 20:25:35 +0000

    Deploy docs to GitHub Pages from commit bc448bf5b3
    Commit: bc448bf5b38e1d7af5b5cbda5b9d01f0e268c0b6
    GitHub Actions run: 2776749075

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -8,6 +8,7 @@ var searchData=
   ['ahtx0_5fcmd_5fcalibrate',['AHTX0_CMD_CALIBRATE',['../_adafruit___a_h_t_x0_8h.html#af777eecac85a33b92e770a29831b81dd',1,'Adafruit_AHTX0.h']]],
   ['ahtx0_5fcmd_5fsoftreset',['AHTX0_CMD_SOFTRESET',['../_adafruit___a_h_t_x0_8h.html#a39366d95fae372068381f14f9cd71a64',1,'Adafruit_AHTX0.h']]],
   ['ahtx0_5fcmd_5ftrigger',['AHTX0_CMD_TRIGGER',['../_adafruit___a_h_t_x0_8h.html#a664271358e4c3fcb030eed58e8055a5f',1,'Adafruit_AHTX0.h']]],
+  ['ahtx0_5fi2caddr_5falternate',['AHTX0_I2CADDR_ALTERNATE',['../_adafruit___a_h_t_x0_8h.html#a9e36ff4fc896875b138fcbda5d5e2dcd',1,'Adafruit_AHTX0.h']]],
   ['ahtx0_5fi2caddr_5fdefault',['AHTX0_I2CADDR_DEFAULT',['../_adafruit___a_h_t_x0_8h.html#a3800113c6fa8958588bda681b0200a6f',1,'Adafruit_AHTX0.h']]],
   ['ahtx0_5fstatus_5fbusy',['AHTX0_STATUS_BUSY',['../_adafruit___a_h_t_x0_8h.html#a1ebc248b94bb3a063448c7ec734a8506',1,'Adafruit_AHTX0.h']]],
   ['ahtx0_5fstatus_5fcalibrated',['AHTX0_STATUS_CALIBRATED',['../_adafruit___a_h_t_x0_8h.html#a4b6deca30d5d717560ab78cc816d4e12',1,'Adafruit_AHTX0.h']]],
--- html/search/all_2.js
+++ html/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___a_h_t_x0.html#a142dbcfa1e989467f6ac7d0ca8280d39',1,'Adafruit_AHTX0']]]
+  ['begin',['begin',['../class_adafruit___a_h_t_x0.html#a613f6a3bccc1859ec9f34c3ec70fccfe',1,'Adafruit_AHTX0']]]
 ];
